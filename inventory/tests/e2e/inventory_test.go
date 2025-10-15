cac6fd21fdd14edd1b6916d323f36199bc4f9da0
George <92686556+GrudTrigger@users.noreply.github.com>
2025-10-15 13:01:28 +0300

    add e2e test for inventory service method

--- inventory/tests/e2e/inventory_test.go
+++ inventory/tests/e2e/inventory_test.go
@@ -1,1 +1,66 @@
 package integration
+
+import (
+	"context"
+
+	"github.com/onsi/ginkgo/v2"
+	"github.com/onsi/gomega"
+	inventoryV1 "github.com/rocker-crm/shared/pkg/proto/inventory/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var _ = ginkgo.Describe("InvenrotyService", func() {
+	var (
+		ctx             context.Context
+		cancel          context.CancelFunc
+		inventoryClient inventoryV1.InventoryServiceClient
+	)
+
+	ginkgo.BeforeEach(func() {
+		ctx, cancel = context.WithCancel(suiteCtx)
+
+		conn, err := grpc.NewClient(env.App.Address(), grpc.WithTransportCredentials(insecure.NewCredentials()))
+		gomega.Expect(err).ToNot(gomega.HaveOccurred(), "ожидали успешное подключение к grpc приложению")
+		inventoryClient = inventoryV1.NewInventoryServiceClient(conn)
+	})
+
+	ginkgo.AfterEach(func() {
+		err := env.ClearPartsCollection(ctx)
+		gomega.Expect(err).ToNot(gomega.HaveOccurred(), "ожидали успешную очистку коллекции parts")
+		cancel()
+	})
+
+	ginkgo.Describe("Get part for uuid", func() {
+		var partUuid string
+		ginkgo.BeforeEach(func() {
+			var err error
+			partUuid, err = env.InsertTestPartData(ctx)
+			gomega.Expect(err).ToNot(gomega.HaveOccurred(), "ожидали успешную вставку тестовой запчасти в mongoDB")
+		})
+
+		ginkgo.It("должен успешно получить запчасть по uuid", func() {
+			resp, err := inventoryClient.GetPart(ctx, &inventoryV1.GetPartRequest{Uuid: partUuid})
+			gomega.Expect(err).ToNot(gomega.HaveOccurred())
+			gomega.Expect(resp.GetPart()).ToNot(gomega.BeNil())
+			gomega.Expect(resp.GetPart().Uuid).To(gomega.Equal(partUuid))
+		})
+	})
+
+	ginkgo.Describe("Get parts list with filter name", func() {
+		ginkgo.BeforeEach(func() {
+			err := env.InsertTestSlicePartsData(ctx)
+			gomega.Expect(err).ToNot(gomega.HaveOccurred(), "ожидали успешную вставку тестовой запчастей в mongoDB")
+		})
+		ginkgo.It("должен успешно получить запчасть с фильтром name:name-1", func() {
+			resp, err := inventoryClient.ListParts(ctx, &inventoryV1.ListPartsRequest{
+				Filter: &inventoryV1.PartsFilter{
+					Names: []string{"name-1"},
+				},
+			})
+			gomega.Expect(err).ToNot(gomega.HaveOccurred())
+			gomega.Expect(resp.GetParts()).ToNot(gomega.BeNil())
+			gomega.Expect(resp.GetParts()[0].Name).To(gomega.Equal("name-1"))
+		})
+	})
+})
--- inventory/tests/e2e/test_environment.go
+++ inventory/tests/e2e/test_environment.go
@@ -1,1 +1,111 @@
 package integration
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/brianvoe/gofakeit/v7"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+type Category int32
+
+// Вставляет тестовую запчасть в mongo и возвращает ее uuid
+func (env *TestEnvironment) InsertTestPartData(ctx context.Context) (string, error) {
+	partUuid := gofakeit.UUID()
+	now := time.Now()
+
+	dimensions := bson.M{
+		"length": gofakeit.Float64(),
+		"width":  gofakeit.Float64(),
+		"height": gofakeit.Float64(),
+		"weight": gofakeit.Float64(),
+	}
+
+	manufacturer := bson.M{
+		"name":    gofakeit.Company(),
+		"country": gofakeit.Country(),
+		"website": gofakeit.URL(),
+	}
+
+	part := bson.M{
+		"_id":           partUuid,
+		"name":          "engine",
+		"description":   gofakeit.Product().Description,
+		"price":         gofakeit.Float32(),
+		"stockQuantity": gofakeit.Int64(),
+		"category":      Category(1),
+		"dimensions":    dimensions,
+		"manufacturer":  manufacturer,
+		"tags":          []string{"a", "b", "c"},
+		"createdAt":     now,
+	}
+
+	databaseName := os.Getenv("MONGO_DATABASE")
+	if databaseName == "" {
+		databaseName = "inventory"
+	}
+
+	_, err := env.Mongo.Client().Database(databaseName).Collection(inventoryCollectionName).InsertOne(ctx, part)
+	if err != nil {
+		return "", err
+	}
+	return partUuid, nil
+}
+
+func (env *TestEnvironment) InsertTestSlicePartsData(ctx context.Context) error {
+	databaseName := os.Getenv("MONGO_DATABASE")
+	if databaseName == "" {
+		databaseName = "inventory"
+	}
+
+	var parts []interface{}
+	for i := 0; i < 5; i++ {
+		name := fmt.Sprintf("name-%d", i)
+		part := NewPart(name)
+		parts = append(parts, part)
+	}
+
+	_, err := env.Mongo.Client().Database(databaseName).Collection(inventoryCollectionName).InsertMany(ctx, parts)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (env *TestEnvironment) ClearPartsCollection(ctx context.Context) error {
+	databaseName := os.Getenv("MONGO_DATABASE")
+	if databaseName == "" {
+		databaseName = "inventory"
+	}
+
+	_, err := env.Mongo.Client().Database(databaseName).Collection(inventoryCollectionName).DeleteMany(ctx, bson.M{})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func NewPart(name string) bson.M {
+	return bson.M{
+		"_id":           gofakeit.UUID(),
+		"name":          name,
+		"description":   gofakeit.Product().Description,
+		"price":         gofakeit.Float32Range(10, 1000),
+		"stockQuantity": gofakeit.Int64(),
+		"category":      Category(1),
+		"dimensions": bson.M{
+			"width":  gofakeit.Float32Range(1, 10),
+			"height": gofakeit.Float32Range(1, 10),
+			"depth":  gofakeit.Float32Range(1, 10),
+		},
+		"manufacturer": bson.M{
+			"name": gofakeit.Company(),
+			"city": gofakeit.City(),
+		},
+		"tags":      []string{"a", "b", "c"},
+		"createdAt": time.Now(),
+	}
+}
