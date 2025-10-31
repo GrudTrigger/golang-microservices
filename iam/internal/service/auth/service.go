15459f4e19cecc573a87c3786f38241ca57f755d
Георгий <goshkamurzik@yandex.ru>
2025-10-31 13:25:53 +0300

    iam:add service

--- iam/internal/service/auth/service.go
+++ iam/internal/service/auth/service.go
@@ -1,1 +1,12 @@
 package auth
+
+import "github.com/rocket-crm/iam/internal/repository"
+
+type service struct {
+	userDb  repository.UserRepository
+	session repository.SessionRepository
+}
+
+func NewService(userDb repository.UserRepository, session repository.SessionRepository) *service {
+	return &service{userDb: userDb, session: session}
+}
